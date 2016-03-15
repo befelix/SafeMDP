7ee91e0b8225f8402dbeca248ae4e1523eac6439
Matteo Turchetta <matteo.turchetta@yahoo.it>
2016-03-15 15:43:18 +0100

    Expansion of (s,a) pairs implemented directly with booleans

--- src/Rreach.py
+++ src/Rreach.py
@@ -25,8 +25,7 @@ def dynamics(states, action, world_shape):
                  Two dimensional array. Each row contains the (x,y) coordinates
                   of the state that results from applying action to the corresponding row of the input states
     """
-    n = world_shape[0]
-    m = world_shape[1]
+    n, m = world_shape
     next_states = np.copy(states)
     if action == 1:
         next_states[:, 1] += 1
@@ -67,7 +66,31 @@ def mat2vec(states_mat_ind, world_shape):
     return vec_ind.astype(int)
 
 
-def r_reach(S_hat, S, states_ind, world_shape):
+def boolean_dynamics(bool_mat, action, world_shape):
+    start = bool_mat.reshape(world_shape).copy()
+    end = bool_mat.reshape(world_shape).copy()
+    if action == 1:  # moves right by one column
+        end[:, 1:] = start[:, 0:-1]
+        end[:, -1] = np.logical_or(end[:, -1], start[:, -1])
+        end[:, 0] = False
+    elif action == 2:  # moves down by one row
+        end[1:, :] = start[0:-1, :]
+        end[-1, :] = np.logical_or(end[-1, :], start[-1, :])
+        end[0, :] = False
+    elif action == 3:  # moves left by one column
+        end[:, 0:-1] = start[:, 1:]
+        end[:, 0] = np.logical_or(end[:, 0], start[:, 0])
+        end[:, -1] = False
+    elif action == 4:  # moves up by one row
+        end[0:-1, :] = start[1:, :]
+        end[0, :] = np.logical_or(end[0, :], start[0, :])
+        end[-1, :] = False
+    else:
+        raise ValueError("Unknown action")
+    return np.reshape(end, (np.prod(world_shape)))
+
+
+def r_reach(S_hat, S, world_shape):
     """
     Implements the intersection of R^(reach)(S_hat) and S
 
@@ -78,8 +101,6 @@ def r_reach(S_hat, S, states_ind, world_shape):
            n_states x n_action array. Safe set with ergodicity properties from previous iteration.
     S: np.array(dtype=bool)
        n_states x n_action array. Set of points above the safety threshold at current iteration
-    states_ind: np.array
-                n_states x 2 array. State space given with matrix indexing (it means (x,y) coordinates)
     world_shape: shape
                  Tuple that contains the shape of the grid world n x m
 
@@ -99,11 +120,10 @@ def r_reach(S_hat, S, states_ind, world_shape):
 
     # From (s,a) to s
     for action in range(1, S.shape[1]):
-        states = states_ind[S_hat[:, action]]
-        if states.size > 0:
-            next_states_mat_ind = dynamics(states, action, world_shape)
-            next_states_vec_ind = mat2vec(next_states_mat_ind, world_shape)
-            reachable[next_states_vec_ind, 0] = S[next_states_vec_ind, 0]
+        tmp = boolean_dynamics(S_hat[:, action], action, world_shape)
+        reachable[:, 0] = np.logical_or(reachable[:, 0], tmp)
+    reachable[:, 0] = np.logical_and(reachable[:, 0], S[:, 0])
+
     return np.logical_or(reachable, S_hat)
 
 
@@ -123,9 +143,9 @@ def plot_S(S, world_shape):
     -------
     none
     """
-    for action in np.arange(1): # np.arange(S.shape[1]):
+    for action in range(1): # np.arange(S.shape[1]):
         plt.figure(action)
-        plt.imshow(np.reshape(S[:, action], world_shape), origin="lower")
+        plt.imshow(np.reshape(S[:, action], world_shape), origin="lower", interpolation="nearest")
         plt.title("action " + str(action))
     plt.show()
 
@@ -146,9 +166,11 @@ if __name__ == "main":
     print(S)
     S_hat = np.zeros((world_shape[0] * world_shape[1], 5), dtype=bool)
     S_hat[0, 0] = True
-    for i in np.arange(12):
-        S_hat = r_reach(S_hat, S, states_ind, world_shape)
+    print(S_hat)
+    for i in range(10):
+        S_hat = r_reach(S_hat, S, world_shape)
         plot_S(S_hat, world_shape)
         print(S_hat)
+        print(i)
         #input("Press something")
     plt.show()
