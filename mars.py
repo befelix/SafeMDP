51f94672c502b6a17b041204313f3865a1190dfc
Matteo Turchetta <matteo.turchetta@yahoo.it>
2016-05-23 18:37:47 +0200

    Baselines implementation (ugly)

--- mars.py
+++ mars.py
@@ -39,8 +39,22 @@ def check_shortest_path(source, next_sample, G, h, altitudes):
     path_safety = np.all(-np.diff(path_altitudes) >= h)
     # print(-np.diff(path_altitudes) >= h)
     # print(path)
-    return path_safety, path[-1]
+    return path_safety, path[-1], path
 
+
+def path_to_boolean_matrix(path, G, S):
+    bool_mat = np.zeros_like(S, dtype=bool)
+    for i in range(len(path) - 1):
+        prev = path[i]
+        succ = path[i + 1]
+        for _, next_node , data in G.out_edges(nbunch=prev, data=True):
+            if next_node == succ:
+                bool_mat[prev, 0] = True
+                a = data["action"]
+                bool_mat[prev, a] = True
+                break
+    bool_mat[succ, 0] = True
+    return bool_mat
 
 
 # Control plotting and saving
@@ -51,8 +65,12 @@ plot_initial_gp = False
 plot_exploration_gp = False
 plot = plot_map or plot_performance or plot_completeness or plot_initial_gp \
        or plot_exploration_gp
-save_performance = True
-plot_for_paper = False
+save_performance = False
+plot_for_paper = True
+random_experiment = False
+non_safe_experiment = False
+non_ergodic_experiment = False
+no_expanders_exploration = False
 
 # Extract and plot Mars data
 world_shape = (120, 70)#(60, 42)
@@ -137,8 +155,8 @@ S0 = np.copy(S_hat0)
 S0[:, 0] = True
 
 # Initialize for performance
-time_steps = 600
-lengthScale = np.linspace(14., 16., num=5)
+time_steps = 525
+lengthScale = np.linspace(14.5, 16., num=1)
 noise = np.linspace(0.075, 0.11, num=1)
 parameters_shape = (noise.size, lengthScale.size)
 
@@ -149,7 +167,6 @@ S_hat_minus_true_S_hat = np.empty(parameters_shape, dtype=int)
 completeness = np.empty(parameters_shape + (time_steps,), dtype=float)
 dist_from_confidence_interval = np.zeros(parameters_shape + (altitudes.size,),
                                          dtype=float)
-
 # Initialize data for GP
 n_samples = 100
 ind = np.random.choice(range(altitudes.size), n_samples)
@@ -220,18 +237,10 @@ for index_l, length in enumerate(lengthScale):
         source = start
         for i in range(time_steps):
             x.update_sets()
-            # S_hat_minus_S = np.count_nonzero(np.logical_and(x.S_hat, ~x.S))
-            # print(S_hat_minus_S)
             next_sample = x.target_sample()
             x.add_observation(*next_sample)
-            if not x.S_hat[source, 0]:
-                print ("source not in S_hat")
-            if not x.S_hat[next_sample[0], 0]:
-                print ("target not in S_hat")
-            # path_safety, source = check_shortest_path(source, [start, 1],
-            #                                        x.graph, h_hard, altitudes)
-            # print(source)
-            path_safety, source = check_shortest_path(source, next_sample,
+            path_safety, source, path = check_shortest_path(source,
+                                                            next_sample,
                                                       x.graph, h_hard, altitudes)
             unsafe_count += not path_safety
 
@@ -354,7 +363,213 @@ if save_performance:
 
 if plot_for_paper:
     # Plot 2D for paper
-    plot_paper(altitudes, x.S_hat, world_shape)
+    plot_paper(altitudes, x.S_hat, world_shape, './safe_exploration.pdf')
 
     # Plot 3D for paper
-    plot_paper(altitudes, x.S_hat, world_shape, surf=True, coord=coord)
+    # plot_paper(altitudes, x.S_hat, world_shape, surf=True,  coord=coord)
+
+########################## NON SAFE ###########################################
+if non_safe_experiment:
+    kernel = GPy.kern.Matern52(input_dim=2, lengthscale=length,
+                               variance=100.)
+    lik = GPy.likelihoods.Gaussian(variance=sigma_n ** 2)
+    gp = GPy.core.GP(X, Y, kernel, lik)
+    h = -np.tan(np.pi / 9. + np.pi / 36.) * step_size[0]
+    # Define SafeMDP object
+    x = GridWorld(gp, world_shape, step_size, beta, altitudes, h, np.ones_like(
+        S0, dtype=bool), S_hat0, L, update_dist=25)
+
+    # Insert samples from (s, a) in S_hat0 (needs to be more general in
+    # case for the central state not all actions are safe)
+    tmp = np.arange(x.coord.shape[0])
+    s_vec_ind = np.random.choice(tmp[np.all(x.S_hat[:, 1:], axis=1)])
+
+    for i in range(5):
+        x.add_observation(s_vec_ind, 1)
+        x.add_observation(s_vec_ind, 2)
+        x.add_observation(s_vec_ind, 3)
+        x.add_observation(s_vec_ind, 4)
+
+    # Remove samples used for GP initialization
+    x.gp.set_XY(x.gp.X[n_samples:, :], x.gp.Y[n_samples:])
+
+    t = time.time()
+    unsafe_count = 0
+    source = start
+    trajectory = []
+    unsafe = False
+    for i in range(time_steps):
+        x.update_sets()
+        next_sample = x.target_sample()
+        x.add_observation(*next_sample)
+        path_safety, source, path = check_shortest_path(source, next_sample,
+                                                      x.graph, h_hard, altitudes)
+        if path_safety:
+            trajectory = trajectory + path
+        else:
+            for j in range(len(path) - 1):
+                prev = path[j]
+                succ = path[j + 1]
+                if altitudes[prev] - altitudes[succ] >= h_hard:
+                    trajectory = trajectory + [prev]
+                else:
+                    unsafe = True
+                    break
+        # Performance
+        # trajectory = np.unique(trajectory)
+        # visited = np.zeros_like(S0, dtype=bool)
+        # visited[trajectory, 0] = True
+        visited = path_to_boolean_matrix(trajectory, x.graph, S0)
+        coverage = 100 * np.count_nonzero(np.logical_and(visited,
+                                                    true_S_hat_epsilon))/max_size
+        false_safe = np.count_nonzero(np.logical_and(x.S_hat, ~true_S_hat))
+
+        # Store and print
+        completeness[index_n, index_l, i] = coverage
+        print(coverage, false_safe, unsafe_count, i)
+        if unsafe:
+            break
+    plot_paper(altitudes, visited, world_shape, './no_safe_exploration.pdf')
+    x.plot_S(visited)
+
+############################### RANDOM ########################################
+if random_experiment:
+    kernel = GPy.kern.Matern52(input_dim=2, lengthscale=length,
+                               variance=100.)
+    lik = GPy.likelihoods.Gaussian(variance=sigma_n ** 2)
+    gp = GPy.core.GP(X, Y, kernel, lik)
+    # Define SafeMDP object
+    x = GridWorld(gp, world_shape, step_size, beta, altitudes, h, np.ones_like(
+        S0, dtype=bool), S_hat0, L, update_dist=25)
+
+    source = start
+    trajectory = []
+    unsafe = False
+    for i in range(time_steps):
+        a = np.random.choice([1, 2, 3, 4])
+        for _, next_node, data in x.graph.out_edges(nbunch=source, data=True):
+            if data["action"] == a:
+                h_prev = altitudes[source]
+                h_succ = altitudes[next_node]
+                source = next_node
+                if h_prev - h_succ >= h_hard:
+                    trajectory = trajectory + [next_node]
+                else:
+                    unsafe = True
+                    break
+        if unsafe:
+            break
+    # trajectory = np.unique(trajectory)
+    # visited = np.zeros_like(S0, dtype=bool)
+    # visited[trajectory, 0] = True
+    visited = path_to_boolean_matrix(trajectory, x.graph, S0)
+    coverage = 100 * np.count_nonzero(np.logical_and(visited,
+                                                true_S_hat_epsilon))/max_size
+    false_safe = np.count_nonzero(np.logical_and(x.S_hat, ~true_S_hat))
+    print(coverage, i)
+    plot_paper(altitudes, visited, world_shape, './rand_exploration.pdf')
+    x.plot_S(visited)
+
+############################# NON ERGODIC #####################################
+if non_ergodic_experiment:
+    kernel = GPy.kern.Matern52(input_dim=2, lengthscale=length,
+                               variance=100.)
+    lik = GPy.likelihoods.Gaussian(variance=sigma_n ** 2)
+    gp = GPy.core.GP(X, Y, kernel, lik)
+    h = -np.tan(np.pi / 9. + np.pi / 36.) * step_size[0]
+
+    # Need to remove expanders otherwise next sample will be in G and
+    # therefore in S_hat before I can set S_hat = S
+    L_non_ergodic = 100.
+
+    # Define SafeMDP object
+    x = GridWorld(gp, world_shape, step_size, beta, altitudes, h, S0, S_hat0,
+                  L_non_ergodic, update_dist=25)
+
+    # Insert samples from (s, a) in S_hat0 (needs to be more general in
+    # case for the central state not all actions are safe)
+    tmp = np.arange(x.coord.shape[0])
+    s_vec_ind = np.random.choice(tmp[np.all(x.S_hat[:, 1:], axis=1)])
+
+    for i in range(5):
+        x.add_observation(s_vec_ind, 1)
+        x.add_observation(s_vec_ind, 2)
+        x.add_observation(s_vec_ind, 3)
+        x.add_observation(s_vec_ind, 4)
+
+    # Remove samples used for GP initialization
+    x.gp.set_XY(x.gp.X[n_samples:, :], x.gp.Y[n_samples:])
+
+    # Simulation loop
+    t = time.time()
+    unsafe_count = 0
+    source = start
+    for i in range(time_steps):
+        x.update_sets()
+        x.S_hat = x.S.copy()
+        next_sample = x.target_sample()
+        x.add_observation(*next_sample)
+        try:
+            path_safety, source, path = check_shortest_path(source, next_sample,
+                                                  x.graph, h_hard, altitudes)
+        except Exception:
+            print ("No safe path available")
+            break
+        unsafe_count += not path_safety
+
+    x.S_hat[:, 0] = np.any(x.S_hat[:, 1:], axis=1)
+    # Performance
+    coverage = 100 * np.count_nonzero(np.logical_and(x.S_hat,
+                                                true_S_hat_epsilon))/max_size
+
+    # Store and print
+    print(coverage, unsafe_count, i)
+    plot_paper(altitudes, x.S_hat, world_shape,
+               './no_ergodic_exploration.pdf')
+    x.plot_S(x.S_hat)
+
+################################## NO EXPANDERS ###############################
+if no_expanders_exploration:
+    kernel = GPy.kern.Matern52(input_dim=2, lengthscale=length,
+                               variance=100.)
+    lik = GPy.likelihoods.Gaussian(variance=sigma_n ** 2)
+    gp = GPy.core.GP(X, Y, kernel, lik)
+    h = -np.tan(np.pi / 9. + np.pi / 36.) * step_size[0]
+
+    L_no_expanders = 100.
+
+    x = GridWorld(gp, world_shape, step_size, beta, altitudes, h, S0, S_hat0,
+                  L_no_expanders, update_dist=25)
+
+    # Insert samples from (s, a) in S_hat0 (needs to be more general in
+    # case for the central state not all actions are safe)
+    tmp = np.arange(x.coord.shape[0])
+    s_vec_ind = np.random.choice(tmp[np.all(x.S_hat[:, 1:], axis=1)])
+
+    for i in range(5):
+        x.add_observation(s_vec_ind, 1)
+        x.add_observation(s_vec_ind, 2)
+        x.add_observation(s_vec_ind, 3)
+        x.add_observation(s_vec_ind, 4)
+
+    # Remove samples used for GP initialization
+    x.gp.set_XY(x.gp.X[n_samples:, :], x.gp.Y[n_samples:])
+
+    source = start
+    unsafe_count = 0
+    for i in range(int(time_steps)):
+        x.update_sets()
+        next_sample = x.target_sample()
+        x.add_observation(*next_sample)
+        path_safety, source, path = check_shortest_path(source,
+                                                            next_sample,
+                                                      x.graph, h_hard, altitudes)
+        unsafe_count += not path_safety
+        # Performance
+        coverage = 100 * np.count_nonzero(np.logical_and(x.S_hat,
+                                                    true_S_hat_epsilon))/max_size
+        false_safe = np.count_nonzero(np.logical_and(x.S_hat, ~true_S))
+        print(coverage, false_safe, unsafe_count, i)
+    plot_paper(altitudes, x.S_hat, world_shape, './no_G_exploration.pdf')
+    x.plot_S(x.S_hat)
+
