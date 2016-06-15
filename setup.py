ae37c48acda3e451b86d6493ca3a2db9d636ade5
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-06-15 09:09:18 +0200

    Move experiments to separate folder

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """Read the name relative to current directory"""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="safemdp",
+    version="1.0",
+    author="Matteo Turchetta, Felix Berkenkamp",
+    author_email="matteotu@ethz.ch, befelix@ethz.ch",
+    description=("Safe exploration in MDPs"),
+    license="MIT",
+    url="http://packages.python.org/an_example_pypi_project",
+    packages=['safemdp'],
+    long_description=read('README.md'),
+    install_requires=[
+        'GPy >= 0.8.0',
+        'numpy >= 1.7.2',
+        'scipy >= 0.16',
+        'matplotlib >= 1.5',
+        'networkx >= 1.1',
+    ],
+)
