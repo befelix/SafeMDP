78686a178e363e908f37f6b7e1977176fa55208d
Felix Berkenkamp <fberkenkamp@gmail.com>
2016-06-16 09:37:31 +0200

    Add __all__ to __init__.py

--- safemdp/__init__.py
+++ safemdp/__init__.py
@@ -4,5 +4,10 @@ from .SafeMDP_class import *
 from .utilities import *
 from .grid_world import *
 
+# Add everything to __all__
+__all__ = [s for s in dir() if not s.startswith('_')]
+
+# Import test after __all__ (no documentation)
 from numpy.testing import Tester
 test = Tester().test
+
